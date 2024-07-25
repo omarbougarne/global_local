c1482a29163e82dde2874c4063fdd9af400bc99b
omarbougarne <omarwimu@gmail.com>
2024-07-25 11:12:08 +0100

    global scale

---
+++ globallocal/src/globallocal/Dice.java
@@ -0,0 +1,19 @@
+package globallocal;
+
+import java.util.Random;
+
+public class Dice {
+
+Random random;
+int number;
+
+Dice(){
+random = new Random();
+roll();
+}
+
+void roll() {
+number = random.nextInt(6)+1;
+System.out.println(number);
+}
+}
---
+++ globallocal/src/globallocal/GlobalLocal.java
@@ -0,0 +1,19 @@
+package globallocal;
+
+
+public class GlobalLocal {
+
+public static void main(String[] args) {
+
+//local =  declared inside a method
+//   visible only to that method
+
+//global  = declared outside a method, but within a class
+//   visible to all parts of a class
+
+Dice diceRoller = new Dice();
+
+}
+}
+
+
